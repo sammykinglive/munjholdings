56d260c8ac2b499e7e7eacf47bcf3c7c09cd8264
Samuel Lartey <86780084+sammykinglive@users.noreply.github.com>
2025-02-01 12:26:12 +0000

    Add files via upload

--- login  script.js
+++ login  script.js
@@ -1,17 +1,4 @@
-function submitLoginForm() {
-    var email = document.getElementById("email-input").value;
-    var password = document.getElementById("password-input").value;
-  
-    // Perform your validation logic here
-    // Replace the following condition with your actual validation logic
-    if (email === "user@holdings.com" && password === "admin") {
-      // Redirect to the private.html page upon successful login
-      window.location.href = "private2.html";
-    } else {
-      // Display an error message or perform other actions for failed login
-      alert("Invalid email or password. Please try again.");
-    }
-  }
+
 
 
   function submitLoginForm() {
@@ -38,7 +25,7 @@ function submitLoginForm() {
     // Replace the following condition with your actual validation logic
     if (email === "user2@holdings.com" && password === "admin") {
       // Redirect to the private.html page upon successful login
-      window.location.href = "private2.html";
+      window.location.href = "dashboard/private2.html";
     } else {
       // Display an error message or perform other actions for failed login
       alert("Invalid email or password. Please try again.");
@@ -46,5 +33,39 @@ function submitLoginForm() {
   }
 
 
+
+
+
+//private 3 origional
+  function submitLoginForm() {
+    var email = document.getElementById("email-input").value;
+    var password = document.getElementById("password-input").value;
   
+    // Perform your validation logic here
+    // Replace the following condition with your actual validation logic
+    if (email === "user3@holdings.com" && password === "admin") {
+      // Redirect to the private.html page upon successful login
+      window.location.href = "dashboard/private3.html";
+    } else {
+      // Display an error message or perform other actions for failed login
+      alert("Invalid email or password. Please try again.");
+    }
+  }
   
+
+//private 4
+function submitLoginForm() {
+  var email = document.getElementById("email-input").value;
+  var password = document.getElementById("password-input").value;
+
+  // Perform your validation logic here
+  // Replace the following condition with your actual validation logic
+  if (email === "user4@holdings.com" && password === "admin") {
+    // Redirect to the private.html page upon successful login
+    window.location.href = "dashboard/private4.html";
+  } else {
+    // Display an error message or perform other actions for failed login
+    alert("Invalid email or password. Please try again.");
+  }
+}
+ 
